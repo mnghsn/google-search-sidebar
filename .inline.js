cfaddb7ed57ff0844aeb3028932e6852770ad630
Jimmy Lin <jmlntw@gmail.com>
2023-04-05 08:25:05 +0800

    Fix `ERR_REQUIRE_ESM` error

--- .inline.js
+++ .inline.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       name: 'indent',
       transform (file, content, size = '2', indent = ' ') {
-        const indentString = require('indent-string')
+        const indentString = (...args) => import('indent-string').then(({default: indentString}) => indentString(...args))
         return indentString(content, parseInt(size, 10), { indent: indent })
       }
     }
